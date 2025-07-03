9c67b30d2ce42bb27fa89e36c24efd3dd47730dd
clover <cloverphp@qq.com>
2025-07-03 19:00:08 +0800

    添加测试路由

--- app/home/main.go
+++ app/home/main.go
@@ -1,11 +1,11 @@
 package home
 
 import (
-	"net/http"
-
 	"github.com/duxweb/go-fast/app"
 	"github.com/duxweb/go-fast/route"
 	"github.com/labstack/echo/v4"
+
+	"dux-project/app/home/routes"
 )
 
 var config = struct {
@@ -26,8 +26,14 @@ func Init(t *app.Dux) {
 
 func Register(t *app.Dux) {
 	group := route.Get("web")
+	routes.RouteWeb(group)
+
 	group.Get("/test", func(c echo.Context) error {
-		return c.String(http.StatusOK, "Hello, World!")
+		// return c.String(http.StatusOK, "Hello, World!")
+		return c.JSON(200, map[string]interface{}{
+			"name": "dux",
+			"age":  10,
+		})
 	}, "web.home")
 
 }
--- app/home/routes/web.go
+++ app/home/routes/web.go
@@ -1,11 +1,15 @@
 package routes
 
 import (
+	"dux-project/app/home/web"
+
 	"github.com/duxweb/go-fast/route"
 	"github.com/labstack/echo/v4"
 )
 
 func RouteWeb(router *route.RouterData) {
+	router.Get("/home/index", web.Index, "home.index")
+
 	router.Get("/", func(ctx echo.Context) error {
 		return ctx.JSON(200, "dsadsad")
 	}, "首页")
